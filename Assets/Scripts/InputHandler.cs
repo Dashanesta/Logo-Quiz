0c53c11c09d8251e0e2f3f7c398634f2b94ef837
Dashanesta <71793564+Dashanesta@users.noreply.github.com>
2021-09-20 18:34:02 -0400

    Answer working!

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,11 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
+using TMPro;
 
 public class InputHandler : MonoBehaviour
 {
+    public TMP_InputField inputField;
+    private string input;
+    public string logoName;
+
     public void OnInput()
     {
-        
+        input = inputField.text;
+        if (String.Compare(input, logoName, true, CultureInfo.CurrentCulture) == 0)
+        {
+            print("yay");
+        }
+        else
+        {
+            print(input);
+        }
     }
 }
