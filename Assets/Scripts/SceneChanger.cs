0dcf9e20bc3b43b7572fb2a3d7276afc1f82ed93
Dashanesta <71793564+Dashanesta@users.noreply.github.com>
2021-09-25 21:41:11 -0400

    more levels and scrolling

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
@@ -108,4 +107,149 @@ public class SceneChanger : MonoBehaviour
        level = 13;
        LoadLevel();
    }
+   
+   public void Level15()
+   {
+       level = 14;
+       LoadLevel();
+   }
+   
+   public void Level16()
+   {
+       level = 15;
+       LoadLevel();
+   }
+   
+   public void Level17()
+   {
+       level = 16;
+       LoadLevel();
+   }
+   
+   public void Level18()
+   {
+       level = 17;
+       LoadLevel();
+   }
+   
+   public void Level19()
+   {
+       level = 18;
+       LoadLevel();
+   }
+   
+   public void Level20()
+   {
+       level = 19;
+       LoadLevel();
+   }
+   
+   public void Level21()
+   {
+       level = 20;
+       LoadLevel();
+   }
+   
+   public void Level22()
+   {
+       level = 21;
+       LoadLevel();
+   }
+   
+   public void Level23()
+   {
+       level = 22;
+       LoadLevel();
+   }
+   
+   public void Level24()
+   {
+       level = 23;
+       LoadLevel();
+   }
+   
+   public void Level25()
+   {
+       level = 24;
+       LoadLevel();
+   }
+   
+   public void Level26()
+   {
+       level = 25;
+       LoadLevel();
+   }
+   
+   public void Level27()
+   {
+       level = 26;
+       LoadLevel();
+   }
+   
+   public void Level28()
+   {
+       level = 27;
+       LoadLevel();
+   }
+   
+   public void Level29()
+   {
+       level = 28;
+       LoadLevel();
+   }
+   
+   public void Level30()
+   {
+       level = 29;
+       LoadLevel();
+   }
+   
+   public void Level31()
+   {
+       level = 30;
+       LoadLevel();
+   }
+   
+   public void Level32()
+   {
+       level = 31;
+       LoadLevel();
+   }
+   
+   public void Level33()
+   {
+       level = 32;
+       LoadLevel();
+   }
+   
+   public void Level34()
+   {
+       level = 33;
+       LoadLevel();
+   }
+   
+   public void Level35()
+   {
+       level = 34;
+       LoadLevel();
+   }
+   
+   public void Level36()
+   {
+       level = 35;
+       LoadLevel();
+   }
+   
+   public void Level37()
+   {
+       level = 36;
+       LoadLevel();
+   }
+   
+   public void Level38()
+   {
+       level = 37;
+       LoadLevel();
+   }
+   
 }
